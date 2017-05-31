99f9529cfe55021194893ac93532fe9d6d008809
Mikhail Golod <mikhail.golod@gmail.com>
2017-05-31 11:28:39 +0300

    prettier added

--- init.el
+++ init.el
@@ -3,6 +3,9 @@
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
+(push "~/.emacs.d/vendor" load-path)
+
+;; Install use-package
 (package-install 'use-package t)
 
 ;; Core editor
@@ -47,10 +50,18 @@
   :bind (("C->" . mc/mark-next-like-this)
 	 ("C-<" . mc/mark-previous-like-this)))
 
+(use-package prettier-js
+  :config
+  (setq prettier-js-args '("--trailing-comma" "es5"
+			   "--single-quote" "true"
+			   "--max-width" "120"
+			   )))
+
 (use-package js2-mode
   :ensure t
   :mode ("\\.js\\'" . js2-mode)
-  :interpreter ("node" . js2-mode))
+  :interpreter ("node" . js2-mode)
+  :config (add-hook 'js2-mode-hook 'prettier-js-mode))
 
 (use-package less-css-mode :ensure t)
 
